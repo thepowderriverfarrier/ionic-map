1ad0bdc398da86cc88b45a0560413099cfcd8d2e
thepowderriverfarrier <thepowderriverfrrier@Daniels-MBP.fios-router.home>
2017-09-26 10:27:03 -0400

    ionic-map
    reactive ionic-map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+//stops the error when calling google.maps below
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef:ElementRef;
 
+  constructor(public navCtrl: NavController) {
   }
+
+  ionViewDidLoad(){
+    this.showMap();
+  }
+
+  showMap(){
+    //needs location
+    const location = new google.maps.LatLng(38.857, -77.009003);
+  
+    //set up map options
+    const options = {
+      center: location,
+      zoom: 10,
+      //streetViewControl: false,
+      mapTypeId: 'hybrid'
+    }
+
+    const  map = new google.maps.Map(this.mapRef.nativeElement, options);
+  
+    this.addMarker(location, map);
+  }
+
+    addMarker(position, map){
+      return new google.maps.Marker({
+        position, 
+        map
+      })
+    }
 
 }
